9cff32d1556ff658f702c9a51078ba2de1ce31f6
ItsVeed <45048948+ItsVeed@users.noreply.github.com>
2021-07-05 16:31:40 +0100

    removed quest stopping in CooksAssistant. Started on romeo and juliet.

--- src/Quests/RomeoAndJuliet.java
+++ src/Quests/RomeoAndJuliet.java
@@ -11,43 +11,55 @@ public class RomeoAndJuliet {
     private boolean giveLetterToRomeo = true;
     private boolean talkToLawrence = true;
     private boolean talkToApothecary = true;
-    private boolean goUpToJuliet2 = true;
     private boolean givePotionToJuliet = true;
 
     public RomeoAndJuliet(APIContext ctx) { this.ctx = ctx; }
 
     public void main() {
-        // If quest is finished move on to next quest
-        if (ctx.quests().isCompleted(IQuestAPI.Quest.ROMEO_AND_JULIET)) {
-            return;
+
+        System.out.println(getStage(IQuestAPI.Quest.ROMEO_AND_JULIET));
+    }
+
+    private int getStage(IQuestAPI.Quest quest){
+        if(quest.getVarPlayer() != null){
+            return ctx.vars().getVarp(quest.getVarPlayer().getId());
+        } else if(quest.getVarbit() != null){
+            return ctx.vars().getVarbit(quest.getVarbit().getId());
         }
-        // If quest is not started start quest
-        if (!ctx.quests().isStarted(IQuestAPI.Quest.ROMEO_AND_JULIET)) {
-            return;
-        }
-        // If need to talk to romeo talk to him
-        if (talkToRomeo) {
-            return;
-        }
-        // If need to talk to juliet talk to her
-        if (talkToJuliet) {
-            return;
-        }
-        // If need to give letter to romeo give him the letter
-        if (giveLetterToRomeo) {
-            return;
-        }
-        // If need to talk to lawrence talk to him
-        if (talkToLawrence) {
-            return;
-        }
-        // If need to talk to apothecary talk to him
-        if (talkToApothecary) {
-            return;
-        }
-        // If need to give potion to juliet give her the potion
-        if (givePotionToJuliet) {
-            return;
-        }
+        return -1;
     }
 }
+
+
+//// If quest is finished move on to next quest
+//        if (ctx.quests().isCompleted(IQuestAPI.Quest.ROMEO_AND_JULIET)) {
+//                return;
+//                }
+//                // If quest is not started start quest
+//                if (!ctx.quests().isStarted(IQuestAPI.Quest.ROMEO_AND_JULIET)) {
+//                return;
+//                }
+//                // If need to talk to romeo talk to him
+//                if (talkToRomeo) {
+//                return;
+//                }
+//                // If need to talk to juliet talk to her
+//                if (talkToJuliet) {
+//                return;
+//                }
+//                // If need to give letter to romeo give him the letter
+//                if (giveLetterToRomeo) {
+//                return;
+//                }
+//                // If need to talk to lawrence talk to him
+//                if (talkToLawrence) {
+//                return;
+//                }
+//                // If need to talk to apothecary talk to him
+//                if (talkToApothecary) {
+//                return;
+//                }
+//                // If need to give potion to juliet give her the potion
+//                if (givePotionToJuliet) {
+//                return;
+//                }
