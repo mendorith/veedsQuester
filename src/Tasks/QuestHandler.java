e1135f9dadd408d6f59d8b878cc9ed0eb41135eb
ItsVeed <45048948+ItsVeed@users.noreply.github.com>
2021-07-08 17:48:59 +0100

    Removed not needed files.

--- src/Tasks/QuestHandler.java
+++ src/Tasks/QuestHandler.java
@@ -8,19 +8,14 @@ public class QuestHandler {
     APIContext ctx;
 
     CooksAssistant cooksAssistant;
-    DemonSlayer demonSlayer;
-    TheRestlessGhost theRestlessGhost;
     RomeoAndJuliet romeoAndJuliet;
-    SheepShearer sheepShearer;
 
     public QuestHandler(APIContext ctx) {
         this.ctx = ctx;
 
         this.cooksAssistant = new CooksAssistant(ctx);
-        this.demonSlayer = new DemonSlayer(ctx);
-        this.theRestlessGhost = new TheRestlessGhost(ctx);
         this.romeoAndJuliet = new RomeoAndJuliet(ctx);
-        this.sheepShearer = new SheepShearer(ctx);
+
     }
 
     public void main() {
@@ -28,14 +23,8 @@ public class QuestHandler {
             switch (Vars.currentQuest) {
                 case COOKS_ASSISTANT : {
                     cooksAssistant.main();
-                } case DEMON_SLAYER: {
-                    demonSlayer.main();
-                } case THE_RESTLESS_GHOST: {
-                    theRestlessGhost.main();
                 } case ROMEO_AND_JULIET: {
                     romeoAndJuliet.main();
-                } case SHEEP_SHEARER: {
-                    sheepShearer.main();
                 }
             }
         } else {
