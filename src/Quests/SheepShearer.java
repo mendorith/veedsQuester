e0d9ee13d196641ed728406f9b00c96c62f480a5
Mendorith <goatman865@yahoo.com>
2021-07-12 21:29:13 -0400

    Completed Sheep Shearer Quest.

--- src/Quests/SheepShearer.java
+++ src/Quests/SheepShearer.java
@@ -2,6 +2,8 @@ package Quests;
 
 import com.epicbot.api.shared.APIContext;
 import com.epicbot.api.shared.entity.NPC;
+import com.epicbot.api.shared.entity.SceneObject;
+import com.epicbot.api.shared.entity.WidgetGroup;
 import com.epicbot.api.shared.methods.IQuestAPI;
 import com.epicbot.api.shared.model.Area;
 import com.epicbot.api.shared.model.Tile;
@@ -14,12 +16,15 @@ public class SheepShearer extends Quest {
         super(ctx);
     }
 
-    Area Fred_House = new Area(3188, 3275, 3192, 3270);
-    Area Sheep_Pasture = new Area(3193, 3276, 3204, 3267);
+    Area Fred_House = new Area(3184, 3278, 3192, 3270);
+    Area Sheep_Pasture = new Area(3193, 3276, 3204, 3260);
     Area Spinning_Wheel = new Area(1, 3208, 3214, 3211, 3212);
 
     @Override
     public void main() {
+        NPC sheep = ctx.npcs().query().id(2786,2699,2693).results().nearest();
+        SceneObject Door = ctx.objects().query().id(13001).actions("Open").located(new Tile(3189, 3275, 0)).results().nearest();
+        SceneObject upstairsDoor = ctx.objects().query().id(1543).actions("Open").located(new Tile(3207, 3214, 1)).results().nearest();
         if (ctx.quests().isCompleted(IQuestAPI.Quest.SHEEP_SHEARER))
             Vars.currentQuest = null;
 
@@ -27,24 +32,48 @@ public class SheepShearer extends Quest {
 
         switch (getStage(IQuestAPI.Quest.SHEEP_SHEARER)) {
             case 0:
-                if (!ctx.inventory().contains(1735)) {
-                    pickupItem(Fred_House, 1735);
-                } else {
-                    String[] chatOptions = {"I'm looking for a quest.", "Yes."};
-                    talkTo(732, Fred_House, chatOptions);
-                }
+                Vars.State = "Starting the Quest.";
+                if (Door != null) {
+                    Door.interact();
+                    return;}
+                String[] chatOptions = {"I'm looking for a quest.", "Yes."};
+                talkTo(732, Fred_House, chatOptions);
                 break;
             case 1:
-                NPC sheep = ctx.npcs().query().id(2768,2699,2693).results().nearest();
-                if (ctx.inventory().getCount(1737) < 20) {
-                    if (sheep != null) {
-                        if (sheep.interact("Shear"))
-                            Time.sleep(3_000, () -> ctx.inventory().contains(1737));
-                    } ctx.webWalking().walkTo(Sheep_Pasture.getCentralTile());
-                } else if (ctx.inventory().getCount(1759) < 20) {
-                    interactObject(Spinning_Wheel, 14669, "Spin");
 
+                if (ctx.inventory().getCount(1759) < 20) {
+                    if (!ctx.inventory().contains(1735)) {
+                        Vars.State = "Getting some Shears.";
+                        if (Door != null)
+                            Door.interact();
+                        else pickupItem(Fred_House,1735);
+                    } else if (ctx.inventory().getCount(1737) < 20 && !ctx.inventory().contains(1759)) {
+                        Vars.State = "Shearing the sheep.";
+                        if (sheep != null && sheep.canReach(ctx)) {
+                            sheep.interact();
+                            Time.sleep(3_000, () -> !ctx.localPlayer().isAnimating());
+                        } ctx.webWalking().walkTo(Sheep_Pasture.getCentralTile());
+                    } else {
+                        Vars.State = "Spinning the Wool.";
+                        interactObject(Spinning_Wheel, 14889, "Spin");
+                        ctx.keyboard().holdKey(32, 40_000, ctx.inventory().getCount(1759) == 20);
+                    }
+                } else{
+                    Vars.State = "Turning in the Quest.";
+                    if(upstairsDoor != null)
+                        upstairsDoor.interact();
+                    else {
+                        if (Door != null)
+                            Door.interact();
+                        else {
+                            String[] turnIn = {"I need to talk to you about shearing these sheep!", "That's the last of them."};
+                            talkTo(732, Fred_House, turnIn);
+                        }
+                    }
                 }
+            case 20:
+                if (ctx.dialogues().canContinue())
+                    ctx.dialogues().selectContinue();
         }
     }
 }
